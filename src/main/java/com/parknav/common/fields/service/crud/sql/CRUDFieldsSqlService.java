dac01513e64dea18c7b3822f7d9c4b012c751f91
Gordan Krešić <gordan@parknav.com>
2022-08-15 19:04:19 +0200

    Switch from common-fields to nar

--- src/main/java/com/parknav/common/fields/service/crud/sql/CRUDFieldsSqlService.java
+++ src/main/java/com/parknav/common/fields/service/crud/sql/CRUDFieldsSqlService.java
@@ -1,18 +1,18 @@
 package com.parknav.common.fields.service.crud.sql;
 
 import com.google.common.collect.Sets;
-import com.parknav.common.fields.FieldEnum;
-import com.parknav.common.fields.FieldGraph;
-import com.parknav.common.fields.FieldUnavailableException;
-import com.parknav.common.fields.HasEntityFields;
-import com.parknav.common.fields.service.crud.CRUDException;
-import com.parknav.common.fields.service.crud.CRUDFieldsService;
 import com.parknav.common.sql.DMLColumn;
 import com.parknav.common.sql.statement.DeleteStatement;
 import com.parknav.common.sql.statement.InsertStatement;
 import com.parknav.common.sql.statement.SelectStatement;
 import com.parknav.common.sql.statement.UpdateStatement;
 import com.parknav.common.sql.where.WhereTerm;
+import com.steatoda.nar.FieldUnavailableException;
+import com.steatoda.nar.NarEntity;
+import com.steatoda.nar.NarField;
+import com.steatoda.nar.NarGraph;
+import com.steatoda.nar.service.crud.NarCRUDException;
+import com.steatoda.nar.service.crud.NarCRUDService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,7 +30,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-public abstract class CRUDFieldsSqlService<I, C extends HasEntityFields<I, C, F>, F extends Enum<F> & FieldEnum, S> implements CRUDFieldsService<I, C, F, S> {
+public abstract class CRUDFieldsSqlService<I, C extends NarEntity<I, C, F>, F extends Enum<F> & NarField, S> implements NarCRUDService<I, C, F, S> {
 
 	public static final int DefaultQueryFetchSize = 100;
 
@@ -45,7 +45,7 @@ public abstract class CRUDFieldsSqlService<I, C extends HasEntityFields<I, C, F>
 	public Set<F> getModifiableFields() { return Collections.unmodifiableSet(modifiableFields); }
 
 	@Override
-	public C get(I id, FieldGraph<F> graph) {
+	public C get(I id, NarGraph<F> graph) {
 
 		try (PreparedStatement statement = new SelectStatement(tableName)
 			.addTableDQLColumns(getDQLColumns(graph), this::decorateDQLColumn)
@@ -56,13 +56,13 @@ public abstract class CRUDFieldsSqlService<I, C extends HasEntityFields<I, C, F>
 				return resultSet.next() ? resolveResultSet(resultSet, graph) : null;
 			}
 		} catch (SQLException e) {
-			throw new CRUDException("Unable to find entity with id " + id, e);
+			throw new NarCRUDException("Unable to find entity with id " + id, e);
 		}
 
 	}
 
 	@Override
-	public void create(C entity, FieldGraph<F> graph) {
+	public void create(C entity, NarGraph<F> graph) {
 
 		checkMandatoryFields(entity);
 
@@ -70,19 +70,19 @@ public abstract class CRUDFieldsSqlService<I, C extends HasEntityFields<I, C, F>
 			if (statement.execute())
 				try (ResultSet resultSet = statement.getResultSet()) {
 					if (!resultSet.next())
-						throw new CRUDException("Unable to insert new entity into db!");
+						throw new NarCRUDException("Unable to insert new entity into db!");
 					readResultSet(entity, resultSet, graph);
 				}
 		} catch (SQLException e) {
 			// note: can't log entity itself because it doesn't have even ID (and most toString methods defaults to logging ID)
-			throw new CRUDException("Unable to create entity", e);
+			throw new NarCRUDException("Unable to create entity", e);
 		}
 
 		Log.trace("Created {}", entity);
 
 	}
 
-	protected InsertStatement buildCreateStatement(C entity, FieldGraph<F> graph) {
+	protected InsertStatement buildCreateStatement(C entity, NarGraph<F> graph) {
 		return new InsertStatement(tableName)
 			.addDMLColumns(getDMLColumns(entity))
 			.addTableDQLColumns(getDQLColumns(graph), this::decorateDQLColumn);
@@ -98,12 +98,12 @@ public abstract class CRUDFieldsSqlService<I, C extends HasEntityFields<I, C, F>
 	 * @param graph return graph
 	 */
 	@Override
-	public void modify(C entity, C patch, FieldGraph<F> graph) {
+	public void modify(C entity, C patch, NarGraph<F> graph) {
 
 		// NOTE: be carefully not to modify patch, because it may hold data valuable to caller (e.g. regarding modifying sub-entites etc.)
 
 		patch = patch.cloneFlat();
-		patch.intersect(FieldGraph.of(modifiableFields));
+		patch.intersect(NarGraph.of(modifiableFields));
 
 		UpdateStatement updateStatement = buildModifyStatement(entity, patch, graph);
 
@@ -116,11 +116,11 @@ public abstract class CRUDFieldsSqlService<I, C extends HasEntityFields<I, C, F>
 			if (statement.execute())
 				try (ResultSet resultSet = statement.getResultSet()) {
 					if (!resultSet.next())
-						throw new CRUDException("Unable to update entity " + entity);
+						throw new NarCRUDException("Unable to update entity " + entity);
 					readResultSet(entity, resultSet, graph);
 				}
 		} catch (SQLException e) {
-			throw new CRUDException("Unable to modify entity " + entity, e);
+			throw new NarCRUDException("Unable to modify entity " + entity, e);
 		}
 
 		Log.trace("Modified {}", entity);
@@ -128,7 +128,7 @@ public abstract class CRUDFieldsSqlService<I, C extends HasEntityFields<I, C, F>
 	}
 
 	/**
-	 * Constructs statement used in {@link #modify(HasEntityFields, HasEntityFields, FieldGraph)}.
+	 * Constructs statement used in {@link #modify(NarEntity, NarEntity, NarGraph)}.
 	 *
 	 * @param entity entity to modify
 	 * @param patch patch describing attributes to modify
@@ -136,7 +136,7 @@ public abstract class CRUDFieldsSqlService<I, C extends HasEntityFields<I, C, F>
 	 *
 	 * @return {@link UpdateStatement} or {@code null}, if there's nothing to update/modify
 	 */
-	protected UpdateStatement buildModifyStatement(C entity, C patch, FieldGraph<F> graph) {
+	protected UpdateStatement buildModifyStatement(C entity, C patch, NarGraph<F> graph) {
 
 		List<DMLColumn> dmlColumns = getDMLColumns(patch);
 
@@ -159,7 +159,7 @@ public abstract class CRUDFieldsSqlService<I, C extends HasEntityFields<I, C, F>
 		) {
 			statement.executeUpdate();
 		} catch (SQLException e) {
-			throw new CRUDException("Unable to delete entity " + entity, e);
+			throw new NarCRUDException("Unable to delete entity " + entity, e);
 		}
 
 		Log.trace("Deleted {}", entity);
@@ -183,10 +183,10 @@ public abstract class CRUDFieldsSqlService<I, C extends HasEntityFields<I, C, F>
 		try {
 			// NOTE: don't put statement inside try-with-resources since it's needed for streaming results
 			PreparedStatement statement = selectStatement.build(getConnection());
-			return StreamSupport.stream(new FieldsStatementSpliterator<>(statement, this::resolveResultSet, FieldGraph.of(fields)), false)
+			return StreamSupport.stream(new FieldsStatementSpliterator<>(statement, this::resolveResultSet, NarGraph.of(fields)), false)
 				.peek(entity -> entity.setId(null));	// #getDQLColumns probably added id and #readResultSet read it, but it's meaningless
 		} catch (SQLException e) {
-			throw new CRUDException("Unable to list all values for fields " + fields, e);
+			throw new NarCRUDException("Unable to list all values for fields " + fields, e);
 		}
 
 	}
@@ -201,17 +201,17 @@ public abstract class CRUDFieldsSqlService<I, C extends HasEntityFields<I, C, F>
 		) {
 			try (ResultSet resultSet = statement.executeQuery()) {
 				if (!resultSet.next())
-					throw new CRUDException("Query did not return any rows?!!");
+					throw new NarCRUDException("Query did not return any rows?!!");
 				return resultSet.getInt(1);
 			}
 		} catch (SQLException e) {
-			throw new CRUDException("Unable to count entities", e);
+			throw new NarCRUDException("Unable to count entities", e);
 		}
 
 	}
 
 	@Override
-	public Stream<C> query(S selector, FieldGraph<F> graph) {
+	public Stream<C> query(S selector, NarGraph<F> graph) {
 
 		return doQuery(buildQueryStatement(selector, graph), graph);
 
@@ -220,17 +220,17 @@ public abstract class CRUDFieldsSqlService<I, C extends HasEntityFields<I, C, F>
 	public int getQueryFetchSize() { return queryFetchSize; }
 	public void setQueryFetchSize(int queryFetchSize) { this.queryFetchSize = queryFetchSize; }
 
-	protected SelectStatement buildQueryStatement(S selector, FieldGraph<F> graph) {
+	protected SelectStatement buildQueryStatement(S selector, NarGraph<F> graph) {
 		return buildQueryStatement(selector, graph, SelectStatement::new);
 	}
 
-	protected SelectStatement buildQueryStatement(S selector, FieldGraph<F> graph, Function<String, SelectStatement> selectStatementBuilder) {
+	protected SelectStatement buildQueryStatement(S selector, NarGraph<F> graph, Function<String, SelectStatement> selectStatementBuilder) {
 		return selectStatementBuilder.apply(tableName)
 			.addTableDQLColumns(getDQLColumns(graph), this::decorateDQLColumn)
 			.addWhereTerms(getSelectorWhereTerm(selector));
 	}
 
-	protected Stream<C> doQuery(SelectStatement select, FieldGraph<F> graph) {
+	protected Stream<C> doQuery(SelectStatement select, NarGraph<F> graph) {
 
 		try {
 			// NOTE: don't put statement inside try-with-resources since it's needed for streaming results
@@ -238,7 +238,7 @@ public abstract class CRUDFieldsSqlService<I, C extends HasEntityFields<I, C, F>
 			statement.setFetchSize(queryFetchSize);
 			return StreamSupport.stream(new FieldsStatementSpliterator<>(statement, this::resolveResultSet, graph), false);
 		} catch (SQLException e) {
-			throw new CRUDException("Unable to list entities", e);
+			throw new NarCRUDException("Unable to list entities", e);
 		}
 
 	}
@@ -249,7 +249,7 @@ public abstract class CRUDFieldsSqlService<I, C extends HasEntityFields<I, C, F>
 	protected abstract String getColumn(F field);
 	protected abstract WhereTerm getIdentityWhereTerm(I id);
 	protected abstract WhereTerm getSelectorWhereTerm(S selector);
-	protected abstract void readResultSet(C entity, ResultSet resultSet, FieldGraph<F> graph) throws SQLException;
+	protected abstract void readResultSet(C entity, ResultSet resultSet, NarGraph<F> graph) throws SQLException;
 
 	/**
 	 * Returns {@link Connection}, throwing {@link RuntimeException} instead of {@link SQLException} in case of error.
@@ -283,7 +283,7 @@ public abstract class CRUDFieldsSqlService<I, C extends HasEntityFields<I, C, F>
 			throw new FieldUnavailableException(missingFields);
 	}
 
-	protected C resolveResultSet(ResultSet resultSet, FieldGraph<F> graph) throws SQLException {
+	protected C resolveResultSet(ResultSet resultSet, NarGraph<F> graph) throws SQLException {
 		C entity = instance();
 		readResultSet(entity, resultSet, graph);
 		return entity;
--- src/main/java/com/parknav/common/fields/service/crud/sql/FieldsStatementSpliterator.java
+++ src/main/java/com/parknav/common/fields/service/crud/sql/FieldsStatementSpliterator.java
@@ -1,22 +1,22 @@
 package com.parknav.common.fields.service.crud.sql;
 
-import com.parknav.common.fields.FieldEnum;
-import com.parknav.common.fields.FieldGraph;
-import com.parknav.common.fields.HasFields;
 import com.parknav.common.sql.StatementSpliterator;
+import com.steatoda.nar.NarField;
+import com.steatoda.nar.NarGraph;
+import com.steatoda.nar.NarObject;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class FieldsStatementSpliterator<C extends HasFields<C, F>, F extends Enum<F> & FieldEnum> extends StatementSpliterator<C> {
+public class FieldsStatementSpliterator<C extends NarObject<C, F>, F extends Enum<F> & NarField> extends StatementSpliterator<C> {
 
 	@FunctionalInterface
-	public interface FieldsResolver<C extends HasFields<C, F>, F extends Enum<F> & FieldEnum> {
-		C resolve(ResultSet resultSet, FieldGraph<F> graph) throws SQLException;
+	public interface FieldsResolver<C extends NarObject<C, F>, F extends Enum<F> & NarField> {
+		C resolve(ResultSet resultSet, NarGraph<F> graph) throws SQLException;
 	}
 
-	public FieldsStatementSpliterator(PreparedStatement statement, FieldsResolver<C, F> fieldsResolver, FieldGraph<F> graph) {
+	public FieldsStatementSpliterator(PreparedStatement statement, FieldsResolver<C, F> fieldsResolver, NarGraph<F> graph) {
 		super(statement, resultSet -> fieldsResolver.resolve(resultSet, graph));
 	}
 
